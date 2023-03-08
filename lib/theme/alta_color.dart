a30ff32c2c9c3f945b468e93e74d9d113c188817
Kharis Azhar <muhammad.azhar@amartha.com>
2023-03-08 20:08:34 +0700

    add material color

--- lib/theme/alta_color.dart
+++ lib/theme/alta_color.dart
@@ -1,9 +1,28 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class AltaColor {
-  /// TODO() : NEXT
-  // MaterialColor
-  static const yellowWaxPepper = Color(0xffF27708);
+
+  static const MaterialColor orange = MaterialColor(
+      _orangePrimaryValue,
+      <int, Color>{
+    500: Color(_orangePrimaryValue),
+    600: Color(0xffC97E3D)
+  });
+
+  /// Hex to Flutter Color :
+  /// 0xff + HEXColor(E27E30)
+  /// 0xffE27E30
+  static const _orangePrimaryValue = 0xffE27E30;
+
+  static const MaterialColor yellow = MaterialColor(
+      _yellowPrimaryValue,
+      <int, Color>{
+        400: Color(0xffEDC547),
+        500: Color(_yellowPrimaryValue),
+      });
+
+  static const _yellowPrimaryValue = 0xffE6C042;
+
+  static const Color black = Color(0xff252229);
+  static const Color white = Color(0xffEDEDEE);
 }
